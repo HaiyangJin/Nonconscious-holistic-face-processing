8987f384620cf745754e72c8887e19b297d227f2
Haiyang <hjin317@aucklanduni.ac.nz>
2019-01-21 12:58:04 +1300

    Updated DataAnalysis_CF_104_6.rmd to Version 3.0
    Updated DataAnalysis_CF_104_6.rmd to Version 3.0 
    run Linear Mixed Model (reduced from Maximum)

---
+++ fit_glmm_nesi_S101.R
@@ -0,0 +1,109 @@
+# The current R file is run in NeSI to get the results for several models and save them as .RData file
+
+#############################  Preparation  ##############################
+# set the working directory
+message("Setting the current working directory...")
+setwd("S101/")
+
+# load libraries
+message("")
+message("Loading the libraries...")
+library(readr)
+library(lme4)
+library(lmerTest)
+
+# load the data file
+message("")
+message("Loading the data file...")
+load("df_clean_S101.RData")
+
+
+#############################  Fitting the full model  ##############################
+# # fit the full model
+# message("")
+# message(paste0(strrep("#", 80)))
+# message("Fitting the full model...")
+# glmm.full <- glmer(isCorrect ~ Experiment * Congruency * Alignment + 
+#                      
+#                      (1 + Experiment_D + Congruency_D + Alignment_D + 
+#                         Experiment_Congruency + Experiment_Alignment + Congruency_Alignment +
+#                         Experiment_Congruency_Alignment | Participant) +
+#                      
+#                      (1 + Experiment_D + Congruency_D + Alignment_D + 
+#                         Experiment_Congruency + Experiment_Alignment + Congruency_Alignment +
+#                         Experiment_Congruency_Alignment | Stimuli),
+#                    
+#                    data = df.clean,
+#                    family = "binomial",
+#                    verbose = TRUE,
+#                    control=glmerControl(optCtrl=list(maxfun=1e6)))
+# 
+# # Save the glmm.full
+# print("Saving the glmm.full ...")
+# save(glmm.full, file = "S101_glmm_full.RData")
+
+
+#############################  Fitting the reduced1 model  ##############################
+# fit the reduced1 model
+message("")
+message(paste0(strrep("#", 80)))
+message("Fitting the reduced1 model...")
+glmm.reduced1 <- glmer(isCorrect ~ Experiment * Congruency * Alignment + 
+                         
+                         (0 + Experiment_D + 
+                            Experiment_Alignment + Congruency_Alignment +
+                            Experiment_Congruency_Alignment | Participant) +
+                         
+                         (1 + Congruency_D + Alignment_D +
+                            Experiment_Congruency + Congruency_Alignment +
+                            Experiment_Congruency_Alignment | Stimuli),
+                       
+                       data = df.clean,
+                       family = "binomial",
+                       verbose = TRUE,
+                       control=glmerControl(optCtrl=list(maxfun=1e6)))
+
+# Save the glmm.reduced1
+print("Saving the glmm.reduced1 ...")
+save(glmm.full, file = "S101_glmm_reduced1.RData")
+
+
+#############################  Fitting the reduced2 model  ##############################
+# fit the reduced2 model
+message("")
+message(paste0(strrep("#", 80)))
+message("Fitting the reduced2 model...")
+glmm.reduced2 <- glmer(isCorrect ~ Experiment * Congruency * Alignment +
+                         
+                         (0 + Experiment_D +
+                            Experiment_Congruency_Alignment | Participant) +
+                         
+                         (1 + Congruency_D + Alignment_D +
+                            Congruency_Alignment +
+                            Experiment_Congruency_Alignment | Stimuli),
+                       
+                       data = df.clean,
+                       family = "binomial",
+                       verbose = TRUE,
+                       control=glmerControl(optCtrl=list(maxfun=1e6)))
+
+# Save the glmm.reduced1
+print("Saving the glmm.reduced1 ...")
+save(glmm.full, file = "S101_glmm_reduced1.RData")
+
+
+
+
+# # step for full model
+# print("Reducing the full model...")
+# lmm.raw.full.step <- step(lmm.raw.full)
+# 
+# # Reduced model
+# lmm.raw.reduced <- get_model(lmm.raw.full.step)
+
+# Save the variables
+# print("Saving the variables...")
+# save(glmm.full, file = "S101_full_fit.RData")
+
+# versions of packages used
+sessionInfo()
