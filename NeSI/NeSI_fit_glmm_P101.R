6cd837efbb62766b2875504badc2abf60307ff83
Haiyang <hjin317@aucklanduni.ac.nz>
2019-01-21 16:53:47 +1300

    update file in Mac

--- NeSI/NeSI_fit_glmm_P101.R
+++ NeSI/NeSI_fit_glmm_P101.R
@@ -1,63 +1,109 @@
 # The current R file is run in NeSI to get the results for several models and save them as .RData file
-message("Fitting model for DataAnalysis_CF_104_6.Rmd...")
-jobid = Sys.getenv("SLURM_JOB_ID")
-message(paste0("The corresponding *.out file is ", jobid, ".out"))
 
 #############################  Preparation  ##############################
 # set the working directory
 message("Setting the current working directory...")
-setwd("P101/")
+setwd("S101/")
 
 # load libraries
 message("")
 message("Loading the libraries...")
-library(tidyverse)
+library(readr)
 library(lme4)
 library(lmerTest)
 
 # load the data file
 message("")
 message("Loading the data file...")
-load("P101_104_106_clean.RData")
+load("df_clean_S101.RData")
 
 
-#############################  Fitting the glmm model for CFS ##############################
-# # fit the model for CFS
+#############################  Fitting the full model  ##############################
+# # fit the full model
 # message("")
 # message(paste0(strrep("#", 80)))
-# message("Fitting the model for CFS...")
-# glmm.cfs <- glmer(isCorrect ~ Congruency * Alignment +
-#                       (1 + Congruency * Alignment | Participant) +
-#                       (1 + Congruency * Alignment | Stimuli),
-#                   data = filter(df.clean, Experiment == "CFS"),
-#                   family = "binomial",
-#                   verbose = TRUE,
-#                   control=glmerControl(optCtrl=list(maxfun=1e6))
-#                   )
+# message("Fitting the full model...")
+# glmm.full <- glmer(isCorrect ~ Experiment * Congruency * Alignment + 
+#                      
+#                      (1 + Experiment_D + Congruency_D + Alignment_D + 
+#                         Experiment_Congruency + Experiment_Alignment + Congruency_Alignment +
+#                         Experiment_Congruency_Alignment | Participant) +
+#                      
+#                      (1 + Experiment_D + Congruency_D + Alignment_D + 
+#                         Experiment_Congruency + Experiment_Alignment + Congruency_Alignment +
+#                         Experiment_Congruency_Alignment | Stimuli),
+#                    
+#                    data = df.clean,
+#                    family = "binomial",
+#                    verbose = TRUE,
+#                    control=glmerControl(optCtrl=list(maxfun=1e6)))
 # 
-# # Save the glmm.cfs
-# print("Saving the glmm.cfs ...")
-# save(glmm.cfs, file = "P101_glmm_cfs.RData")
+# # Save the glmm.full
+# print("Saving the glmm.full ...")
+# save(glmm.full, file = "S101_glmm_full.RData")
 
 
-#############################  Fitting the glmm model for monocular  ##############################
-# fit the model for monocular
+#############################  Fitting the reduced1 model  ##############################
+# fit the reduced1 model
 message("")
 message(paste0(strrep("#", 80)))
-message("Fitting the model for monocular...")
-glmm.mono <- glmer(isCorrect ~ Congruency * Alignment +
-                       (1 + Congruency * Alignment | Participant) +
-                       (1 + Congruency * Alignment | Stimuli),
-                   data = filter(df.clean, Experiment == "monocular"),
-                   family = "binomial",
-                   verbose = TRUE,
-                   control=glmerControl(optCtrl=list(maxfun=1e6))
-                   )
+message("Fitting the reduced1 model...")
+glmm.reduced1 <- glmer(isCorrect ~ Experiment * Congruency * Alignment + 
+                         
+                         (0 + Experiment_D + 
+                            Experiment_Alignment + Congruency_Alignment +
+                            Experiment_Congruency_Alignment | Participant) +
+                         
+                         (1 + Congruency_D + Alignment_D +
+                            Experiment_Congruency + Congruency_Alignment +
+                            Experiment_Congruency_Alignment | Stimuli),
+                       
+                       data = df.clean,
+                       family = "binomial",
+                       verbose = TRUE,
+                       control=glmerControl(optCtrl=list(maxfun=1e6)))
 
-# Save the glmm.mono
-print("Saving the glmm.mono ...")
-save(glmm.mono, file = "P101_glmm_mono.RData")
+# Save the glmm.reduced1
+print("Saving the glmm.reduced1 ...")
+save(glmm.full, file = "S101_glmm_reduced1.RData")
 
+
+#############################  Fitting the reduced2 model  ##############################
+# fit the reduced2 model
+message("")
+message(paste0(strrep("#", 80)))
+message("Fitting the reduced2 model...")
+glmm.reduced2 <- glmer(isCorrect ~ Experiment * Congruency * Alignment +
+                         
+                         (0 + Experiment_D +
+                            Experiment_Congruency_Alignment | Participant) +
+                         
+                         (1 + Congruency_D + Alignment_D +
+                            Congruency_Alignment +
+                            Experiment_Congruency_Alignment | Stimuli),
+                       
+                       data = df.clean,
+                       family = "binomial",
+                       verbose = TRUE,
+                       control=glmerControl(optCtrl=list(maxfun=1e6)))
+
+# Save the glmm.reduced1
+print("Saving the glmm.reduced1 ...")
+save(glmm.full, file = "S101_glmm_reduced1.RData")
+
+
+
+
+# # step for full model
+# print("Reducing the full model...")
+# lmm.raw.full.step <- step(lmm.raw.full)
+# 
+# # Reduced model
+# lmm.raw.reduced <- get_model(lmm.raw.full.step)
+
+# Save the variables
+# print("Saving the variables...")
+# save(glmm.full, file = "S101_full_fit.RData")
 
 # versions of packages used
 sessionInfo()
