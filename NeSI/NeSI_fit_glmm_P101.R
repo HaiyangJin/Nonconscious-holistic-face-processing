d656b82fd36d515a0e02d4da5d259f0c93c6e5c7
Haiyang <hjin317@aucklanduni.ac.nz>
2019-02-12 17:11:22 +1300

    combine the data from all four experiments for further analysis

--- NeSI/NeSI_fit_glmm_P101.R
+++ NeSI/NeSI_fit_glmm_P101.R
@@ -1,5 +1,5 @@
 # The current R file is run in NeSI to get the results for several models and save them as .RData file
-message("Fitting model for DataAnalysis_CF_104_6.Rmd...")
+message("Fitting model for P101_DataAnalysis.Rmd...")
 jobid = Sys.getenv("SLURM_JOB_ID")
 message(paste0("The corresponding *.out file is ", jobid, ".out"))
 
@@ -18,7 +18,7 @@ library(lmerTest)
 # load the data file
 message("")
 message("Loading the data file...")
-load("P101_104_106_clean.RData")
+load("P101_cf_clean.RData")
 
 
 #############################  Fitting the glmm model for CFS ##############################
@@ -27,57 +27,18 @@ message("")
 message(paste0(strrep("#", 80)))
 message("Fitting the maximum model...")
 
-glmm.max.acc <- glmer(isCorrect ~ Experiment * Congruency * Alignment + 
-                          (1 + Exp_D + Con_D + Ali_D + Exp_Con + Exp_Ali + Con_Ali + Exp_Con_Ali | Participant) +
-                          (1 + Exp_D + Con_D + Ali_D + Exp_Con + Exp_Ali + Con_Ali + Exp_Con_Ali | FaceGroup),
-                      data = df.clean,
+glmm.max.acc <- glmer(isCorrect ~ Task * Congruency * Alignment + ExpCode + 
+                          (1 + Task_D + Con_D + Ali_D + Task_Con + Task_Ali + Con_Ali | Participant) + 
+                          (1 + Task_D + Con_D + Ali_D + Task_Con + Task_Ali + Con_Ali | FaceGroup),
+                      data = df.cf.all,
                       family = "binomial",
-                      verbose = TRUE,
-                      control=glmerControl(optCtrl=list(maxfun=1e6))
+                      # verbose = TRUE,
+                      control = glmerControl(optCtrl = list(maxfun = 1e6))
 )
 
 # Save the maximum model
 print("Saving the glmm.max.acc ...")
-save(glmm.max.acc, file = "E1046_glmm_max_acc.RData")
-
-
-
-#############################  Fitting the glmm model for CFS ##############################
-# # fit the model for CFS
-# message("")
-# message(paste0(strrep("#", 80)))
-# message("Fitting the model for CFS...")
-# glmm.cfs <- glmer(isCorrect ~ Congruency * Alignment +
-#                       (1 + Congruency * Alignment | Participant) +
-#                       (1 + Congruency * Alignment | Stimuli),
-#                   data = filter(df.clean, Experiment == "CFS"),
-#                   family = "binomial",
-#                   verbose = TRUE,
-#                   control=glmerControl(optimizer = "nloptwrap",optCtrl=list(maxfun=1e6))
-#                   )
-# 
-# # Save the glmm.cfs
-# print("Saving the glmm.cfs ...")
-# save(glmm.cfs, file = "P101_glmm_cfs.RData")
-
-
-#############################  Fitting the glmm model for monocular  ##############################
-# # fit the model for monocular
-# message("")
-# message(paste0(strrep("#", 80)))
-# message("Fitting the model for monocular...")
-# glmm.mono <- glmer(isCorrect ~ Congruency * Alignment +
-#                        (1 + Congruency * Alignment | Participant) +
-#                        (1 + Congruency * Alignment | Stimuli),
-#                    data = filter(df.clean, Experiment == "monocular"),
-#                    family = "binomial",
-#                    verbose = TRUE,
-#                    control=glmerControl(optimizer = "nloptwrap",optCtrl=list(maxfun=1e6))
-#                    )
-# 
-# # Save the glmm.mono
-# print("Saving the glmm.mono ...")
-# save(glmm.mono, file = "P101_glmm_mono.RData")
+save(glmm.max.acc, file = "P101_glmm_max_acc.RData")
 
 
 # versions of packages used
