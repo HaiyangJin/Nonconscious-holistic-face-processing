a033fba5719add26746ab45595625e2290667743
Haiyang <hjin317@aucklanduni.ac.nz>
2019-05-13 20:21:22 +1200

    rename _D to _C

--- R/dummy_coding_P101.R
+++ R/dummy_coding_P101.R
@@ -2,15 +2,15 @@ dummy_coding_P101 <- function (df) {
   df <- {
     df %>% 
       mutate(
-        View_D = if_else(Viewing == viewing.levels[1], 0, if_else(Viewing == viewing.levels[2], 1, NaN)),
-        Con_D = if_else(Congruency == congruency.levels[1], 0, if_else(Congruency == congruency.levels[2], 1, NaN)),
-        Ali_D = if_else(Alignment == alignment.levels[1], 0, if_else(Alignment == alignment.levels[2], 1, NaN)),
+        View_C = if_else(Viewing == viewing.levels[1], 0, if_else(Viewing == viewing.levels[2], 1, NaN)),
+        Con_C = if_else(Congruency == congruency.levels[1], 0, if_else(Congruency == congruency.levels[2], 1, NaN)),
+        Ali_C = if_else(Alignment == alignment.levels[1], 0, if_else(Alignment == alignment.levels[2], 1, NaN)),
         
-        View_Con = View_D * Con_D, 
-        View_Ali = View_D * Ali_D,
-        Con_Ali = Con_D * Ali_D,
+        View_Con = View_C * Con_C, 
+        View_Ali = View_C * Ali_C,
+        Con_Ali = Con_C * Ali_C,
         
-        View_Con_Ali = View_D * Con_D * Ali_D
+        View_Con_Ali = View_C * Con_C * Ali_C
       )
   }
   return(df)
