fcbb5383d08bdd82d869938836c62589f8f96431
Haiyang <hjin317@aucklanduni.ac.nz>
2019-07-07 11:49:41 +1200

    output file for paper
    add plots for combined data 1,2 and 3,4

--- Utilities/save_png.R
+++ Utilities/save_png.R
@@ -1,11 +1,18 @@
-  res_value = 350
-  res_rela = 90
-  w = 650/res_rela*res_value
-  h = 402/res_rela*res_value
+# Save the plots for papers
+
+tmpfn <- "E3-CF-RT"
+
+
+res_value = 350
+res_rela = 90
+w = 650/res_rela*res_value
+h = 402/res_rela*res_value
+
+png(file= paste0(tmpfn, ".png") ,width=w,height=h, res = res_value)
+
+#plot
+rt.LinePlot.E3
+
+dev.off()
+
   
-  png(file= paste0(tmpfn, ".png") ,width=w,height=h, res = res_value)
-  
-  #plot
-  d.ColuPlot.E1
-  
-  dev.off()
